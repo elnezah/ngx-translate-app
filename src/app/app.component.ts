8260dcf420c4e903e0010719d90d047b68965f7f
Jose A. Garcés <jgarces@intermedcon.com>
2021-04-18 13:05:43 +0200

    Added Material and FlexLayout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngx-translate-app';
+  private readonly TAG = 'AppComponent';
+
+  public myTestObject = {object: 'Test Object', message: 'Test message'};
 }
