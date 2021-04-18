6bccb92728eedca8995451b4c937dac3618f75ec
Jose A. Garcés <jgarces@intermedcon.com>
2021-04-18 14:04:16 +0200

    Files being correctly uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FileItem, FileUploader } from 'ng2-file-upload';
+
+interface TranslationFile {
+  fileName: string;
+  languageCode: string;
+  content: any;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  private readonly TAG = 'AppComponent';
+export class AppComponent implements OnInit {
+  private static readonly TAG = 'AppComponent';
 
-  public myTestObject = {object: 'Test Object', message: 'Test message'};
+  public hasBaseDropZoneOver: boolean;
+  public fileUploader = new FileUploader({ });
+
+  public files: TranslationFile[];
+
+  constructor(private fileReader: FileReader) {
+  }
+
+  public ngOnInit(): void {
+    this.fileReader.onload = (ev: any) => {
+      try {
+        const mObject = JSON.parse(ev.target.result);
+        console.log(AppComponent.TAG, mObject);
+      } catch (e) {
+        console.warn(AppComponent.TAG, 'error trying to read file', e);
+      }
+    };
+
+    this.fileUploader.onAfterAddingFile = (fi: FileItem) => {
+      console.log(AppComponent.TAG, {fi});
+
+      const fn = fi.file.name;
+      const regexRes = /^([a-zA-Z]{2})\.(json)$/.exec(fn);
+
+      if (regexRes && regexRes.length > 0) {
+        const fileName = regexRes[1];
+        const extension = regexRes[2];
+        this.fileReader.readAsText(fi._file);
+      }
+    };
+  }
+
+  // region Listeners
+  public onFileHooverOnBase($event: any) {
+    this.hasBaseDropZoneOver = $event;
+  }
+
+  public onFileDropOnBase($event: File[]) {
+    console.log(AppComponent.TAG, {$event});
+  }
+
+  // endregion
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
   declarations: [
@@ -10,9 +10,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FileUploadModule
   ],
-  providers: [],
+  providers: [FileReader],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
